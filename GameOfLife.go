1501e670db2d1212c0e64baf54e12466ed48a11f
Firestar99 <31222740+firestar99@users.noreply.github.com>
2018-11-10 14:01:59 +0100

    better Game Of Life

--- GameOfLife.go
+++ GameOfLife.go
@@ -5,12 +5,95 @@ import (
 	"time"
 )
 
-func main() {
-	height := 29
-	width := 29
-	var array = CreateArray(height, width)
-	var next = CreateArray(height, width)
-	ParseLayout(array, height, width, []string{
+type layout struct {
+	height      int
+	width       int
+	generations int
+	initial     []string
+}
+
+var Oscillator1 layout = layout{
+	5,
+	5,
+	10,
+	[]string{
+		"     ",
+		"  #  ",
+		"  #  ",
+		"  #  ",
+		"     ",
+	},
+}
+
+var Glider layout = layout{
+	10,
+	10,
+	24,
+	[]string{
+		"          ",
+		"  #       ",
+		"   #      ",
+		" ###      ",
+		"          ",
+		"          ",
+		"          ",
+		"          ",
+		"          ",
+		"          ",
+	},
+}
+
+var Oscillator2 layout = layout{
+	19,
+	20,
+	30,
+	[]string{
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"       #    #       ",
+		"     ## #### ##     ",
+		"       #    #       ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+		"                    ",
+	},
+}
+
+var SpaceShip layout = layout{
+	11,
+	50,
+	50,
+	[]string{
+		"                                                  ",
+		"                                                  ",
+		"                                                  ",
+		"                                                  ",
+		"                                                  ",
+		" ####    #####    ######                          ",
+		"#   #   #    #   #     #                          ",
+		"    #        #         #                          ",
+		"#  #    #   #    #    #                           ",
+		"          #        ##                             ",
+		"                                                  ",
+	},
+}
+
+var Giant layout = layout{
+	29,
+	29,
+	100,
+	[]string{
 		"                             ",
 		"                             ",
 		"                             ",
@@ -40,17 +123,37 @@ func main() {
 		"                             ",
 		"                             ",
 		"                             ",
-	})
+	},
+}
 
-	for i := 0; i < 100; i++ {
+func main() {
+	var layout layout = Glider
+
+	height := layout.height
+	width := layout.width
+	var array = CreateArray(height, width)
+	var next = CreateArray(height, width)
+	ParseLayout(array, height, width, layout.initial)
+
+	for i := 0; i < layout.generations; i++ {
 		printOut(array, height, width)
-		Step(array, next, height, width)
 
+		//step
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				(*next)[y][x] = Next(array, height, width, y, x)
+			}
+		}
+
+		//swap buffers
 		temp := array
 		array = next
 		next = temp
+
+		//sleep
 		time.Sleep(200 * time.Millisecond)
 	}
+	printOut(array, height, width)
 }
 
 func CreateArray(height, width int) *[][]bool {
@@ -69,33 +172,27 @@ func ParseLayout(array *[][]bool, height, width int, input []string) {
 	}
 }
 
-func Step(array, next *[][]bool, height, width int) {
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			(*next)[y][x] = Next(array, height, width, y, x)
-		}
-	}
-}
-
 func Next(array *[][]bool, height, width int, y, x int) bool {
-	var surrounding int = 0
+	//count the surrounding fields which are alive
+	var count = 0
 	for offy := -1; offy <= 1; offy++ {
 		for offx := -1; offx <= 1; offx++ {
 			if !(offx == 0 && offy == 0) {
 				if ArrayGetOrDefault(array, height, width, offy+y, offx+x, false) {
-					surrounding++
+					count++
 				}
 			}
 		}
 	}
 
-	if surrounding < 2 {
+	//set the new state depending on count
+	if count < 2 {
 		return false
 	}
-	if surrounding == 2 {
+	if count == 2 {
 		return (*array)[y][x]
 	}
-	if surrounding == 3 {
+	if count == 3 {
 		return true
 	}
 	return false
